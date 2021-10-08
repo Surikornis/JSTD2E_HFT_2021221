31604586d438d474f77b7d6ee94f88c2c9b4728c
Surikornis <Surikornis@DESKTOP-PLKEUPG>
2021-10-08 13:12:41 +0200

    Add project files.

---
+++ JSTD2E_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSTD2E_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
