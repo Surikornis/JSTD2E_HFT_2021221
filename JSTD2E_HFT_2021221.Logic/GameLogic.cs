a7b484f9d96435f09fd7ccbb04af35cac438deb5
SurikornisPC <kristof.suri@gmail.com>
2021-11-20 12:11:11 +0100

    GameLogic Non-cruds created

--- JSTD2E_HFT_2021221.Logic/GameLogic.cs
+++ JSTD2E_HFT_2021221.Logic/GameLogic.cs
@@ -45,6 +45,18 @@ namespace JSTD2E_HFT_2021221.Logic
             gameRepo.Update(game);
         }
 
+        // gives back the most expensive game the buyer have purchased
+        public IEnumerable<KeyValuePair<List<Game>, string>> Oldest()
+        {
+            return from x in gameRepo.GetAll()
+                   group x by x.Buyer.Games into g
+                   select new KeyValuePair<List<Game>, string>
+                   (g.Key, g.Max(t => t.GameName));
+        }
 
+        public int Cheapest()
+        {
+            return gameRepo.GetAll().Min(t => t.Price);
+        }
     }
 }
