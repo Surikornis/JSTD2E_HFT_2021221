a3e89b68bddb8da64973829b9442c24671c3703f
SurikornisPC <kristof.suri@gmail.com>
2021-11-30 16:32:57 +0100

    CRUD methods working

--- JSTD2E_HFT_2021221.Client/Program.cs
+++ JSTD2E_HFT_2021221.Client/Program.cs
@@ -16,8 +16,17 @@ namespace JSTD2E_HFT_2021221.Client
             var devteams = rest.Get<DeveloperTeam>("developerteam");
             var games = rest.Get<Game>("game");
 
-            
-            ;
+            // all commented lines work //
+
+            //rest.Post<DeveloperTeam>(new DeveloperTeam()
+            //{
+            //    DevTeam = "TestDev"
+            //}, "developerteam");
+
+            //rest.Delete(6, "/developerteam")
+
+            //double avgprice = rest.GetSingle<double>("stat/avgage");
+            //var expensive = rest.Get<KeyValuePair<string, double>>("stat/expensive");
         }
     }
 }
